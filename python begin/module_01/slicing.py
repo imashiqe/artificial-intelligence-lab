0eaa0278610c94acaf5e42ba382601e4e73e26d3
Ashiqur Rahman <imashiqe@gmail.com>
2025-10-05 13:10:57 +0600

    dictonery data stucture

--- python begin/module_01/slicing.py
+++ python begin/module_01/slicing.py
@@ -1,6 +1,16 @@
-nums = [10, 20, 30, 40, 50, 60]
-print(nums[1:4])   # middle slice
-print(nums[-3:])   # last 3
-print(nums[::2])   # step slice
-print(nums[::-1])  # reverse
-print(nums[:-3])
+# nums = [10, 20, 30, 40, 50, 60]
+# print(nums[1:4])   # middle slice
+# print(nums[-3:])   # last 3
+# print(nums[::2])   # step slice
+# print(nums[::-1])  # reverse
+# print(nums[:-3])
+
+
+phoneBook = {
+     'Alice': '703-493-1834',
+     'Bob': '857-384-1234',
+     'Charlie': '503-493-1834'
+}
+
+
+print(phoneBook['Alice'])
