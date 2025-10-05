a5431721c78751745cebd59dde0e6e1a4e1d0a17
Ashiqur Rahman <imashiqe@gmail.com>
2025-10-05 14:48:09 +0600

    Truthiness

--- python begin/module_01/slicing.py
+++ python begin/module_01/slicing.py
@@ -1,9 +1,9 @@
-# nums = [10, 20, 30, 40, 50, 60]
-# print(nums[1:4])   # middle slice
-# print(nums[-3:])   # last 3
-# print(nums[::2])   # step slice
-# print(nums[::-1])  # reverse
-# print(nums[:-3])
+nums = [10, 20, 30, 40, 50, 60]
+print(nums[1:4])   # middle slice
+print(nums[-3:])   # last 3
+print(nums[::2])   # step slice
+print(nums[::-1])  # reverse
+print(nums[:-3])
 
 
 phoneBook = {
@@ -13,4 +13,9 @@ phoneBook = {
 }
 
 
-print(phoneBook['Alice'])
+# print(phoneBook['Alice'])
+
+
+s = {1,1,2,3,4,4,5,6,7,8,9}
+
+print(s)
