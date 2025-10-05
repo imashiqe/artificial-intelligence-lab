4b749938fc250137741d9bb6ce7a9662a82fbe93
Ashiqur Rahman <imashiqe@gmail.com>
2025-10-05 14:52:02 +0600

    control flow

---
+++ python begin/module_01/controlflow.py
@@ -0,0 +1,9 @@
+# --- DEMO: Control flow ---
+for i in range(10, 51):
+    if i == 0:
+        label = "zero"
+    elif i % 2 == 0:
+        label = "even"
+    else:
+        label = "odd"
+    print(i, "→", label)
